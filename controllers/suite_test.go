53235af9fde0340e58c1debce340a7dff82cbee0
Brent Eagles <beagles@redhat.com>
2022-09-30 14:05:54 -0230

    Remove unused variables
    golangci check catches unused variables.

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -24,9 +24,9 @@ import (
 	. "github.com/onsi/gomega"
 
 //	"k8s.io/client-go/kubernetes/scheme"
-	"k8s.io/client-go/rest"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/envtest"
+//	"k8s.io/client-go/rest"
+//	"sigs.k8s.io/controller-runtime/pkg/client"
+//	"sigs.k8s.io/controller-runtime/pkg/envtest"
 	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
 //	logf "sigs.k8s.io/controller-runtime/pkg/log"
 //	"sigs.k8s.io/controller-runtime/pkg/log/zap"
@@ -38,9 +38,9 @@ import (
 // These tests use Ginkgo (BDD-style Go testing framework). Refer to
 // http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
 
-var cfg *rest.Config
-var k8sClient client.Client
-var testEnv *envtest.Environment
+//var cfg *rest.Config
+//var k8sClient client.Client
+//var testEnv *envtest.Environment
 
 func TestAPIs(t *testing.T) {
 	RegisterFailHandler(Fail)
