da0a7df0e77938f2a6da35b1a3bf227669c368f1
Andrew Bays <abays@redhat.com>
2022-10-20 09:30:54 +0000

    Only add finalizer and update instance if necessary

--- controllers/octaviaapi_controller.go
+++ controllers/octaviaapi_controller.go
@@ -483,10 +483,12 @@ func (r *OctaviaAPIReconciler) reconcileUpgrade(ctx context.Context, instance *o
 func (r *OctaviaAPIReconciler) reconcileNormal(ctx context.Context, instance *octaviav1.OctaviaAPI, helper *helper.Helper) (ctrl.Result, error) {
 	r.Log.Info("Reconciling Service")
 
-	// If the service object doesn't have our finalizer, add it.
-	controllerutil.AddFinalizer(instance, helper.GetFinalizer())
-	// Register the finalizer immediately to avoid orphaning resources on delete
-	if err := r.Update(ctx, instance); err != nil {
+	if !controllerutil.ContainsFinalizer(instance, helper.GetFinalizer()) {
+		// If the service object doesn't have our finalizer, add it.
+		controllerutil.AddFinalizer(instance, helper.GetFinalizer())
+		// Register the finalizer immediately to avoid orphaning resources on delete
+		err := r.Update(ctx, instance)
+
 		return ctrl.Result{}, err
 	}
 
