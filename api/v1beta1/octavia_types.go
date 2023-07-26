fea86331a75d4a2f413204cfc80fa978a4a2a5c1
Dan Prince <dprince@redhat.com>
2023-07-26 13:56:14 -0400

    Make octavia resource plural octavias

--- api/v1beta1/octavia_types.go
+++ api/v1beta1/octavia_types.go
@@ -141,6 +141,7 @@ type OctaviaStatus struct {
 
 //+kubebuilder:object:root=true
 //+kubebuilder:subresource:status
+//+kubebuilder:resource:path=octavias
 //+kubebuilder:printcolumn:name="Status",type="string",JSONPath=".status.conditions[0].status",description="Status"
 //+kubebuilder:printcolumn:name="Message",type="string",JSONPath=".status.conditions[0].message",description="Message"
 
