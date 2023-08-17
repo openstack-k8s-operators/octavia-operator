8d7163b7ac8ea9a79b759679c2e5b432ffd6c5bc
Takashi Kajinami <tkajinam@redhat.com>
2023-08-17 18:02:59 +0900

    Ensure webhook is started
    This ensures webhook is started when controller-manager deployment
    becomes ready.

--- main.go
+++ main.go
@@ -137,20 +137,22 @@ func main() {
 	octaviav1.SetupDefaults()
 
 	// Setup webhooks if requested
+	checker := healthz.Ping
 	if strings.ToLower(os.Getenv("ENABLE_WEBHOOKS")) != "false" {
 		if err = (&octaviav1.Octavia{}).SetupWebhookWithManager(mgr); err != nil {
 			setupLog.Error(err, "unable to create webhook", "webhook", "Octavia")
 			os.Exit(1)
 		}
+		checker = mgr.GetWebhookServer().StartedChecker()
 	}
 
 	//+kubebuilder:scaffold:builder
 
-	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
+	if err := mgr.AddHealthzCheck("healthz", checker); err != nil {
 		setupLog.Error(err, "unable to set up health check")
 		os.Exit(1)
 	}
-	if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
+	if err := mgr.AddReadyzCheck("readyz", checker); err != nil {
 		setupLog.Error(err, "unable to set up ready check")
 		os.Exit(1)
 	}
