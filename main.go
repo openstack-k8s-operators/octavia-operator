c6a7be225c6233ef4dc2600510bb23935ad7cecf
Harald Jensås <hjensas@redhat.com>
2025-05-06 19:17:12 +0200

    Leader election lease tunables
    Expose the leader election tunables for lease duration,
    renew deadline and retry period via environment variables.
    
    This is inspired by rabbitmq operator exposes the same.
    
    Jira: OSPRH-16335
    
    Depends-On: openstack-k8s-operators/lib-common#627

--- main.go
+++ main.go
@@ -43,6 +43,7 @@ import (
 	networkv1 "github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/apis/k8s.cni.cncf.io/v1"
 	rabbitmqv1 "github.com/openstack-k8s-operators/infra-operator/apis/rabbitmq/v1beta1"
 	keystonev1 "github.com/openstack-k8s-operators/keystone-operator/api/v1beta1"
+	"github.com/openstack-k8s-operators/lib-common/modules/common/operator"
 	mariadbv1 "github.com/openstack-k8s-operators/mariadb-operator/api/v1beta1"
 	ovn1beta1 "github.com/openstack-k8s-operators/ovn-operator/api/v1beta1"
 
@@ -96,7 +97,7 @@ func main() {
 		c.NextProtos = []string{"http/1.1"}
 	}
 
-	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+	options := ctrl.Options{
 		Scheme: scheme,
 		Metrics: metricsserver.Options{
 			BindAddress: metricsAddr,
@@ -109,7 +110,15 @@ func main() {
 				Port:    9443,
 				TLSOpts: []func(config *tls.Config){disableHTTP2},
 			}),
-	})
+	}
+
+	err := operator.SetManagerOptions(&options, setupLog)
+	if err != nil {
+		setupLog.Error(err, "unable to set manager options")
+		os.Exit(1)
+	}
+
+	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), options)
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
 		os.Exit(1)
